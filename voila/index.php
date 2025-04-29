586285d93d1037a8756aa01ea1e6ee928604da75
k0880k <124455599+Ig0tU@users.noreply.github.com>
2025-04-29 17:30:13 -0400

    Add voila/index.php 1745962213198

--- voila/index.php
+++ voila/index.php
@@ -5,5 +5,20 @@ use Voila\Core\WordPressToJoomla;
 use Voila\Utils\Logger;
 
 $logger = new Logger();
-$migrator = new WordPressToJoomla($logger);
+
+$wpDbConfig = [
+    'host' => 'your_wordpress_db_host',
+    'dbname' => 'your_wordpress_db_name',
+    'username' => 'your_wordpress_db_user',
+    'password' => 'your_wordpress_db_password'
+];
+
+$joomlaDbConfig = [
+    'host' => 'your_joomla_db_host',
+    'dbname' => 'your_joomla_db_name',
+    'username' => 'your_joomla_db_user',
+    'password' => 'your_joomla_db_password'
+];
+
+$migrator = new WordPressToJoomla($logger, $wpDbConfig, $joomlaDbConfig);
 $migrator->migrate();
